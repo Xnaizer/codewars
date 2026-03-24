7b9642172b80eee6e5d7b7b0803252585072aeda
Xnaizer <seykig028@gmail.com>
2026-03-24 09:11:15 +0700

    number palindrome

---
+++ 34 - number palindrome/soal.js
@@ -0,0 +1,20 @@
+function angkaPalindrome(num) {
+  if(num < 10) return true;
+
+  const numbers = String(num).split('');
+  
+  for(let i = 0; i < Math.floor(numbers.length / 2); i++) {
+    if(numbers[i] === numbers[numbers.length - 1]) {
+        return true;
+    }
+  }
+
+  return false;
+}
+
+// TEST CASES
+console.log(angkaPalindrome(8)); // true
+console.log(angkaPalindrome(10)); // false
+console.log(angkaPalindrome(117)); // false
+console.log(angkaPalindrome(181)); // true
+console.log(angkaPalindrome(1001)); // true
