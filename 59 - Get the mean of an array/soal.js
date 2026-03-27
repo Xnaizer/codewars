b03d814a42f41f73c1deedf3cb223e489ff6799a
Xnaizer <seykig028@gmail.com>
2026-03-27 08:52:42 +0700

    Get the mean of an array

---
+++ 59 - Get the mean of an array/soal.js
@@ -0,0 +1,8 @@
+function getAverage(marks){
+  return Math.floor(marks.reduce((a,b) => a + b, 0) / marks.length);
+}
+
+
+console.log(getAverage([2,2,2,2]));
+console.log(getAverage([1,2,3,4,5,]));
+console.log(getAverage([1,1,1,1,1,1,1,2]));
